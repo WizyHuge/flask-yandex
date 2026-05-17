5c89122d64fd60b70201747e457c0cd229a7012e
WizyHuge <hom.semen@yandex.ru>
2026-05-17 14:20:30 +0500

    Страница /results с параметрами nickname, level, rating

--- solution.py
+++ solution.py
@@ -204,5 +204,31 @@ def choice(planet_name):
 </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+    crossorigin="anonymous">
+    <title>Результаты отбора</title>
+</head>
+<body>
+    <h1>Результаты отбора</h1>
+    <h2>Астронавт: {nickname}</h2>
+    <div class="alert alert-info" role="alert">
+        <h3>Этап: {level}</h3>
+    </div>
+    <div class="alert alert-success" role="alert">
+        <h4>Рейтинг: {rating}</h4>
+    </div>
+</body>
+</html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
