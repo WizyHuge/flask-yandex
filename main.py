5d16caefdbc4a7a870cdefef13c27e7b123def94
WizyHuge <hom.semen@yandex.ru>
2026-05-17 16:30:49 +0500

    flask-sqlalchemy: ORM, модели User/News, регистрация, блог

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import json
 import random
 from flask import Flask, url_for, request, render_template, redirect
 from loginform import LoginForm
+from data import db_session
+from data.users import User
+from data.news import News
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+db_session.global_init("db/blogs.db")
 
 
-@app.route('/')
+@app.route("/")
 @app.route('/index')
 def index():
     title = request.args.get('title', '')
-    return render_template('index.html', title=title)
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index_blog.html", news=news, title=title)
 
 
 @app.route('/member')
@@ -21,6 +28,31 @@ def member():
         crew = json.loads(f.read())
     m = random.choice(crew)
     return render_template('member.html', title='Член экипажа', member=m)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/training/<prof>')
