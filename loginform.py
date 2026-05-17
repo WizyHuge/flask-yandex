d9a9a4414f7df4a245bd75428745bd5a6f63a631
WizyHuge <hom.semen@yandex.ru>
2026-05-17 15:45:12 +0500

    Форма авторизации на корабле с полями капитана и эмблемой

--- loginform.py
+++ loginform.py
@@ -4,7 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    astronaut_id = StringField('Id астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    captain_id = StringField('Id капитана', validators=[DataRequired()])
+    captain_token = PasswordField('Токен капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
