7bdb49b55374c26a1d2ec90e8d3082eb2c8f81d4
WizyHuge <hom.semen@yandex.ru>
2026-05-17 14:00:01 +0500

    Страница /promotion_image с Bootstrap и стилями

--- solution.py
+++ solution.py
@@ -38,5 +38,41 @@ def image_mars():
 </html>"""
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+    crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+    <title>Колонизация</title>
+</head>
+<body>
+    <h1>Жди нас, Марс!</h1>
+    <div class="alert alert-dark">
+        Человечество вырастает из детства.
+    </div>
+    <div class="alert alert-success">
+        Человечеству мала одна планета.
+    </div>
+    <div class="alert alert-warning">
+        Мы сделаем обитаемыми безжизненные пока планеты.
+    </div>
+    <div class="alert alert-info">
+        И начнем с Марса!
+    </div>
+    <div class="alert alert-danger">
+        Присоединяйся!
+    </div>
+    <img src="{url_for('static', filename='img/mars.jpg')}" alt="Марс" width="300">
+</body>
+</html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
