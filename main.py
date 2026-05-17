89662cc6333992b0ed3b178940d1d82e81adf103
WizyHuge <hom.semen@yandex.ru>
2026-05-17 15:05:52 +0500

    flask-wtf

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import os
-from flask import Flask, url_for, request
+import json
+from flask import Flask, url_for, request, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
-def mission():
-    return "Миссия Колонизация Марса"
-
-
 @app.route('/index')
-def motto():
-    return "И на Марсе будут яблони цвести!"
+def index():
+    param = {}
+    param['username'] = "Ученик Яндекс.Лицея"
+    param['title'] = 'Домашняя страница'
+    return render_template('index.html', **param)
+
+
+@app.route('/odd_even')
+def odd_even():
+    return render_template('odd_even.html', number=2)
+
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    return render_template('news.html', news=news_list)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Успех')
 
 
 @app.route('/promotion')
